8ce3df577618aab81f57ff09500f7d24e0cfca9b
gokhangunduz <mail@gokhangunduz.com.tr>
2023-01-23 21:56:01 +0300

    feat(ThemeMode): :tada: Add themeMode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class", '[data-mode="dark"]'],
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
