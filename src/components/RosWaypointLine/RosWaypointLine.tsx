d12b261de524a915a7c9509644a6bb7c17cfafe0
gokhangunduz <mail@gokhangunduz.com.tr>
2023-04-16 17:44:10 +0300

    feat(RobotWaypointLine): :tada: Add first robot location line

--- src/components/RosWaypointLine/RosWaypointLine.tsx
+++ src/components/RosWaypointLine/RosWaypointLine.tsx
@@ -1,19 +1,103 @@
-import React, { Fragment, ReactElement } from "react";
+import React, { Fragment, ReactElement, useEffect, useState } from "react";
 import handleRostoDomMouseCoordinatesConverter from "../../helpers/handleRostoDomMouseCoordinatesConverter";
+import ROSLIB from "roslib";
 
 interface IRosWaypointLine {
+  ros: any;
   activeMission: number;
   missions: any;
   rosMapDetails: any;
 }
 
 export default function RosWaypointLine({
+  ros,
   activeMission,
   missions,
   rosMapDetails,
 }: IRosWaypointLine): ReactElement {
+  const [rosRobotPosition, setRosRobotPosition] = useState<any>();
+
+  const robotPosition = new ROSLIB.Topic({
+    ros: ros,
+    name: "/robot_position",
+    messageType: "geometry_msgs/msg/PoseStamped",
+  });
+
+  useEffect(() => {
+    robotPosition?.subscribe(function (message: any) {
+      setRosRobotPosition({
+        x: message?.pose?.position?.x,
+        y: message?.pose?.position?.y,
+        z: message?.pose?.position?.z,
+      });
+    });
+
+    return () => {
+      robotPosition?.unsubscribe();
+    };
+  });
+
   return (
     <Fragment>
+      {rosRobotPosition?.x && missions[activeMission]?.waypoints[0] && (
+        <div className="absolute inset-0">
+          <svg
+            style={{
+              height: "100%",
+            }}
+          >
+            <line
+              x1={
+                handleRostoDomMouseCoordinatesConverter({
+                  rosMapWebsocketWidth: rosMapDetails?.resolution?.x,
+                  rosMapWebsocketHeight: rosMapDetails?.resolution?.y,
+                  rosMapWidth: rosMapDetails?.x,
+                  rosMapHeight: rosMapDetails?.y,
+                  waypointX: rosRobotPosition?.x,
+                  waypointY: rosRobotPosition?.y,
+                }).x - 6
+              }
+              y1={
+                handleRostoDomMouseCoordinatesConverter({
+                  rosMapWebsocketWidth: rosMapDetails?.resolution?.x,
+                  rosMapWebsocketHeight: rosMapDetails?.resolution?.y,
+                  rosMapWidth: rosMapDetails?.x,
+                  rosMapHeight: rosMapDetails?.y,
+                  waypointX: rosRobotPosition?.x,
+                  waypointY: rosRobotPosition?.y,
+                }).y - 6
+              }
+              x2={
+                handleRostoDomMouseCoordinatesConverter({
+                  rosMapWebsocketWidth: rosMapDetails?.resolution?.x,
+                  rosMapWebsocketHeight: rosMapDetails?.resolution?.y,
+                  rosMapWidth: rosMapDetails?.x,
+                  rosMapHeight: rosMapDetails?.y,
+                  waypointX:
+                    missions[activeMission]?.waypoints[0]?.coordinates?.x,
+                  waypointY:
+                    missions[activeMission]?.waypoints[0]?.coordinates?.y,
+                }).x
+              }
+              y2={
+                handleRostoDomMouseCoordinatesConverter({
+                  rosMapWebsocketWidth: rosMapDetails?.resolution?.x,
+                  rosMapWebsocketHeight: rosMapDetails?.resolution?.y,
+                  rosMapWidth: rosMapDetails?.x,
+                  rosMapHeight: rosMapDetails?.y,
+                  waypointX:
+                    missions[activeMission]?.waypoints[0]?.coordinates?.x,
+                  waypointY:
+                    missions[activeMission]?.waypoints[0]?.coordinates?.y,
+                }).y
+              }
+              stroke="red"
+              strokeWidth="1"
+            />
+          </svg>
+        </div>
+      )}
+
       {activeMission !== -1 &&
         missions[activeMission]?.waypoints?.map(
           (waypoint: any, waypointIndex: number) => {
