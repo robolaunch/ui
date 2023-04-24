124b31b442ba58636c1ef44ef57d87ea744d33c9
gokhangunduz <mail@gokhangunduz.com.tr>
2023-04-24 16:30:02 +0300

    feat(handleGetRandomNumbers): :bug: Add handleGetRandomNumbers function

---
+++ src/helpers/handleGetRandomNumbers.ts
@@ -0,0 +1,5 @@
+export default function handleGetRandomNumbers(length: number): number {
+  const min = 10 ** (length - 1);
+  const max = 10 ** length - 1;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
