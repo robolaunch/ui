a4c544dae82b7ecc539b9dbca209ae7355390770
gokhangunduz <mail@gokhangunduz.com.tr>
2023-04-26 17:53:26 +0300

    refactor(MarketplaceRobotTemplateItem): :boom: Update marketplace item component

--- src/components/MarketplaceRobotTemplateItem/MarketplaceRobotTemplateItem.tsx
+++ src/components/MarketplaceRobotTemplateItem/MarketplaceRobotTemplateItem.tsx
@@ -1,18 +1,21 @@
-import React, { ReactElement } from "react";
-import { MdVerified } from "react-icons/md";
+import React, { Fragment, ReactElement } from "react";
 import CardLayout from "../../layouts/CardLayout";
-import { AiFillStar, AiOutlineStar } from "react-icons/ai";
-import Button from "../Button/Button";
-
+import { BsShieldLockFill } from "react-icons/bs";
+import { MdPublic } from "react-icons/md";
+import { RiOrganizationChart } from "react-icons/ri";
+import InputSelect from "../InputSelect/InputSelect";
+import { Link } from "react-router-dom";
+import stringSlugify from "../../helpers/stringSlugify";
 interface IMarketplaceRobotTemplateItem {
   template: {
     title: string;
     organization: string;
     type: string;
     rosDistro: string;
-    verified: boolean;
     star: boolean;
     starCount: number;
+    access: string | undefined;
+    ownership: boolean;
   };
 }
 
@@ -20,58 +23,112 @@ export default function MarketplaceRobotTemplateItem({
   template,
 }: IMarketplaceRobotTemplateItem): ReactElement {
   return (
-    <CardLayout className="col-span-3 p-8">
-      <div className="flex flex-col gap-8">
-        <div className="flex justify-between" id="header">
-          <div className="flex flex-col gap-2">
-            <div id="name" className="flex gap-1 items-center">
+    <CardLayout className="col-span-3 p-6 hover:scale-[1.032] transition-all duration-500 cursor-pointer !flex !flex-col gap-4">
+      <Link to={stringSlugify(template?.title)}>
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center gap-2">
               <span className="text-sm font-medium">{template?.title}</span>
-              {template?.verified && (
-                <MdVerified className="text-layer-secondary-500" size={16} />
+              {template?.organization === "Robolaunch" && (
+                <img
+                  className="w-6"
+                  src="/images/rocket.svg"
+                  alt="robolaunch"
+                />
               )}
             </div>
-            <span className="text-xs text-layer-dark-300">
-              {template?.organization}
-            </span>
-          </div>
-          <div className="flex flex-col items-center gap-3">
             <span
-              className={`text-[0.60rem] font-medium px-3 py-2 rounded-lg border ${
+              className={`text-[0.60rem] font-medium w-fit ${
                 template?.type === "Hybrid Robot"
-                  ? "text-layer-secondary-500 bg-layer-secondary-50 border-layer-secondary-200"
-                  : "text-layer-primary-500 bg-layer-primary-50 border-layer-primary-200"
+                  ? "text-layer-secondary-500"
+                  : "text-layer-primary-500"
               } `}
             >
               {template?.type}
             </span>
           </div>
+          <div className="flex items-center gap-1 text-[0.60rem] font-medium text-layer-secondary-500 px-1.5 rounded-lg capitalize -ml-1.5">
+            {!template?.ownership ? (
+              <Fragment>
+                <span>Public Template</span>
+                <MdPublic size={16} className="text-layer-secondary-500" />{" "}
+              </Fragment>
+            ) : (
+              <Fragment>
+                <InputSelect className="!p-0 !w-fit !h-8 !border-none text-right">
+                  <Fragment>
+                    <option value={template?.access}>
+                      <span>
+                        {template?.access === "public"
+                          ? "Public"
+                          : template?.access === "organization"
+                          ? template?.organization
+                          : "Private"}{" "}
+                        Template
+                      </span>
+                    </option>
+
+                    {(() => {
+                      switch (template?.access) {
+                        case "public":
+                          return (
+                            <Fragment>
+                              <option value="organization">
+                                {template?.organization} Template
+                              </option>
+                              <option value="private">Private Template</option>
+                            </Fragment>
+                          );
+                        case "organization":
+                          return (
+                            <Fragment>
+                              <option value="public">Public Template</option>
+                              <option value="private">Private Template</option>
+                            </Fragment>
+                          );
+                        case undefined:
+                          return (
+                            <Fragment>
+                              <option value="public">Public Template</option>
+                              <option value="organization">
+                                {template?.organization} Template
+                              </option>
+                            </Fragment>
+                          );
+                      }
+                    })()}
+                  </Fragment>
+                </InputSelect>
+                {template?.access === "public" ? (
+                  <MdPublic size={16} className="text-layer-secondary-500" />
+                ) : template?.access === "organization" ? (
+                  <RiOrganizationChart
+                    size={16}
+                    className="text-layer-secondary-500"
+                  />
+                ) : (
+                  <BsShieldLockFill
+                    size={16}
+                    className="text-layer-secondary-500"
+                  />
+                )}
+              </Fragment>
+            )}
+          </div>
         </div>
-
-        <div className="flex justify-between" id="content">
-          <div>content</div>
-
+        <div className="flex items-center justify-between">
+          <ul className="flex flex-col gap-2 text-xs text-layer-dark-500">
+            <li>ROS Version: ROS2</li>
+            <li>ROS Distro: Humble</li>
+            <li>Created Date: 04.09.2024</li>
+          </ul>
           <img
-            className="w-24"
+            className="w-28"
             src={`/svg/ros/${template?.rosDistro}.svg`}
             alt="ros"
           />
         </div>
-
-        <div className="flex gap-2">
-          <Button
-            className="!h-10 !text-xs !bg-layer-light-50 border border-layer-primary-500 !text-primary hover:!bg-primary hover:!text-layer-light-50 duration-500"
-            text="Import Robot"
-          />
-          <div className="flex items-center gap-2 bg-layer-secondary-50 p-1.5 rounded text-layer-secondary-500 border border-layer-secondary-200">
-            <span className="text-xs">{template?.starCount}</span>
-            {template?.star ? (
-              <AiFillStar size={22} className="text-yellow-300" />
-            ) : (
-              <AiOutlineStar size={22} className="text-yellow-300" />
-            )}
-          </div>
-        </div>
-      </div>
+      </Link>
     </CardLayout>
   );
 }
