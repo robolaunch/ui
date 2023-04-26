512f133017b5262cd5812463fca35a9a7ffe6fc9
gokhangunduz <mail@gokhangunduz.com.tr>
2023-04-26 17:52:01 +0300

    feat(Paginate): :tada: Add paginate component

---
+++ src/components/Paginate/Paginate.tsx
@@ -0,0 +1,61 @@
+import React, { ReactElement, useState } from "react";
+import {
+  MdOutlineKeyboardArrowLeft,
+  MdOutlineKeyboardArrowRight,
+} from "react-icons/md";
+
+interface IPaginate {
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+export default function Paginate({
+  className,
+  style,
+}: IPaginate): ReactElement {
+  const [pageCount, setPageCount] = useState<number>(9);
+  const [currentPage, setCurrentPage] = useState<number>(9);
+
+  function handleNextPage() {
+    if (currentPage < pageCount) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  function handlePrevPage() {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
+  return (
+    <div
+      className={`w-full flex justify-center items-center gap-3 pt-14 ${className}`}
+      style={{ ...style }}
+    >
+      <MdOutlineKeyboardArrowLeft
+        onClick={() => handlePrevPage()}
+        className={`${!currentPage && "invisible"}`}
+      />
+      {Array.from(Array(pageCount + 1).keys()).map((item, index) => {
+        return (
+          <span
+            onClick={() => setCurrentPage(index)}
+            key={index}
+            className={`w-8 h-8 flex items-center justify-center rounded-lg  transition-all duration-300 cursor-pointer ${
+              currentPage === index
+                ? "bg-layer-primary-500 text-layer-light-50"
+                : "hover:bg-layer-primary-50 hover:border hover:border-layer-primary-200"
+            }`}
+          >
+            {item + 1}
+          </span>
+        );
+      })}
+      <MdOutlineKeyboardArrowRight
+        onClick={() => handleNextPage()}
+        className={`${currentPage === pageCount && "invisible"}`}
+      />
+    </div>
+  );
+}
