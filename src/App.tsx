1e3fb0017b9be73ecf686e958c8fd155820c5c71
gokhangunduz <mail@gokhangunduz.com.tr>
2023-04-27 12:40:26 +0300

    refactor(App.tsx): :bug: Add create robot provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,21 @@ import "animate.css";
 import ThemeContext from "./contexts/ThemeContext";
 import "react-toastify/dist/ReactToastify.css";
 import "animate.css";
-//theme
-import "primereact/resources/themes/lara-light-indigo/theme.css";
-//core
-import "primereact/resources/primereact.min.css";
-//icons
-import "primeicons/primeicons.css";
 
 import { ToastContainer } from "react-toastify";
 import { Toaster } from "sonner";
 import SidebarContext from "./contexts/SidebarContext";
+import CreateRobotContext from "./contexts/CreateRobotContext";
 
 function App() {
   return (
     <ThemeContext>
       <SidebarContext>
-        <ToastContainer />
-        <Toaster position="top-center" />
-        <AppRoutes />
+        <CreateRobotContext>
+          <ToastContainer />
+          <Toaster position="top-center" />
+          <AppRoutes />
+        </CreateRobotContext>
       </SidebarContext>
     </ThemeContext>
   );
