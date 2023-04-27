3860fe3bbdce0b26ecd191622a97bdd2b8173bf9
gokhangunduz <mail@gokhangunduz.com.tr>
2023-04-27 12:54:08 +0300

    refactor(CreateRobotContext): :tada: Add robotData fields

--- src/contexts/CreateRobotContext.tsx
+++ src/contexts/CreateRobotContext.tsx
@@ -1,10 +1,29 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
+import { SidebarContext } from "./SidebarContext";
 
 export const CreateRobotContext: any = createContext<any>(null);
 
 // eslint-disable-next-line
 export default ({ children }: any) => {
-  const [robotData, setRobotData] = useState<any>();
+  const { selectedState }: any = useContext(SidebarContext);
+
+  const [robotData, setRobotData] = useState<any>({
+    step1: {
+      name: "",
+      organization: selectedState?.organization,
+      roboticsCloud: selectedState?.roboticsCloud,
+      fleet: selectedState?.fleet,
+      storage: 0,
+      isEnabledIDE: false,
+      isEnabledROS2Bridge: false,
+      VDI: {
+        isEnabled: false,
+        sessionCount: 1,
+      },
+    },
+    step2: {},
+    step3: {},
+  });
 
   return (
     <CreateRobotContext.Provider
