6b161e8f8551f87ae0a308465e0e5db68e0766a6
gokhangunduz <mail@gokhangunduz.com.tr>
2023-04-28 14:29:10 +0300

    feat(CreateRobotFormWorkspaceItem and CreateRobotFormRepositoryItem): :boom: Add workspace and repository item component

---
+++ src/components/CreateRobotFormRepositoryItem/CreateRobotFormRepositoryItem.tsx
@@ -0,0 +1,95 @@
+import React, { ReactElement } from "react";
+import Accordion from "../Accordion/AccordionV2";
+import {
+  IRobotWorkspace,
+  IRobotWorkspaces,
+} from "../../interfaces/robotInterfaces";
+import { FormikProps } from "formik/dist/types";
+import InputText from "../InputText/InputText";
+
+interface ICreateRobotFormRepositoryItem {
+  formik: FormikProps<IRobotWorkspaces>;
+  repository: IRobotWorkspace;
+  repositoryIndex: number;
+  activeRepository: number;
+  setActiveRepository: React.Dispatch<React.SetStateAction<number>>;
+  workspaceIndex: number;
+}
+
+export default function CreateRobotFormRepositoryItem({
+  formik,
+  repository,
+  repositoryIndex,
+  activeRepository,
+  setActiveRepository,
+  workspaceIndex,
+}: ICreateRobotFormRepositoryItem): ReactElement {
+  function handleRemoveRepository(
+    workspaceIndex: number,
+    repositoryIndex: number
+  ) {
+    const temp: any = [...formik.values.workspaces];
+    temp[workspaceIndex].repositories.splice(repositoryIndex, 1);
+    formik.setFieldValue("workspaces", temp);
+  }
+
+  return (
+    <Accordion
+      key={repositoryIndex}
+      id={repositoryIndex}
+      isOpen={activeRepository === repositoryIndex}
+      handleOpen={() => {
+        if (activeRepository === repositoryIndex) {
+          setActiveRepository(-1);
+        } else {
+          setActiveRepository(repositoryIndex);
+        }
+      }}
+      header={
+        <span className="font-medium">
+          {repository.name
+            ? repository?.name + " Repository"
+            : `Repository ${repositoryIndex + 1}`}
+        </span>
+      }
+    >
+      <div className="flex flex-col gap-8 px-3 py-6">
+        <div>
+          <InputText
+            {...formik.getFieldProps(
+              `workspaces.${workspaceIndex}.repositories.${repositoryIndex}.name`
+            )}
+            placeholder="Repository Name"
+            disabled={formik?.isSubmitting}
+          />
+        </div>
+        <div>
+          <InputText
+            {...formik.getFieldProps(
+              `workspaces.${workspaceIndex}.repositories.${repositoryIndex}.url`
+            )}
+            placeholder="Repository URL"
+            disabled={formik?.isSubmitting}
+          />
+        </div>
+        <div>
+          <InputText
+            {...formik.getFieldProps(
+              `workspaces.${workspaceIndex}.repositories.${repositoryIndex}.branch`
+            )}
+            placeholder="Repository Branch"
+            disabled={formik?.isSubmitting}
+          />
+        </div>
+        <span
+          onClick={() => {
+            handleRemoveRepository(workspaceIndex, repositoryIndex);
+          }}
+          className="text-[0.66rem] text-red-500 cursor-pointer mx-auto hover:underline"
+        >
+          Delete {repository?.name ? repository.name : `this`} Repository
+        </span>
+      </div>
+    </Accordion>
+  );
+}
---
+++ src/components/CreateRobotFormWorkspaceItem/CreateRobotFormWorkspaceItem.tsx
@@ -0,0 +1,130 @@
+import React, { Fragment, ReactElement, useState } from "react";
+import Accordion from "../Accordion/AccordionV2";
+import {
+  IRobotWorkspace,
+  IRobotWorkspaces,
+} from "../../interfaces/robotInterfaces";
+import { FormikProps } from "formik/dist/types";
+import InputText from "../InputText/InputText";
+import InputError from "../InputError/InputError";
+import InputSelect from "../InputSelect/InputSelect";
+import { BsPlusCircle } from "react-icons/bs";
+import CreateRobotFormRepositoryItem from "../CreateRobotFormRepositoryItem/CreateRobotFormRepositoryItem";
+
+interface ICreateRobotFormWorkspaceItem {
+  formik: FormikProps<IRobotWorkspaces>;
+  workspace: IRobotWorkspace;
+  workspaceIndex: number;
+}
+
+export default function CreateRobotFormWorkspaceItem({
+  formik,
+  workspace,
+  workspaceIndex,
+}: ICreateRobotFormWorkspaceItem): ReactElement {
+  const [activeWorkspace, setActiveWorkspace] = useState<number>(-1);
+  const [activeRepository, setActiveRepository] = useState<number>(-1);
+
+  function handleAddRepository(workspaceIndex: number): void {
+    const temp: any = [...formik.values.workspaces];
+    temp[workspaceIndex].repositories.push({
+      name: "",
+      url: "",
+      branch: "",
+    });
+    formik.setFieldValue("workspaces", temp);
+  }
+
+  function handleRemoveWorkspace(workspaceIndex: number) {
+    const temp: any = [...formik.values.workspaces];
+    temp.splice(workspaceIndex, 1);
+    formik.setFieldValue("workspaces", temp);
+  }
+
+  return (
+    <Accordion
+      key={workspaceIndex}
+      id={workspaceIndex}
+      isOpen={activeWorkspace === workspaceIndex}
+      handleOpen={() => {
+        if (activeWorkspace === workspaceIndex) {
+          setActiveWorkspace(-1);
+          setActiveRepository(-1);
+        } else {
+          setActiveWorkspace(workspaceIndex);
+        }
+      }}
+      header={
+        <span className="font-medium">
+          {workspace.name
+            ? workspace?.name + " Workspace"
+            : `Workspace ${workspaceIndex + 1}`}
+        </span>
+      }
+    >
+      <div className="flex flex-col gap-7 p-4">
+        <div>
+          <InputText
+            {...formik.getFieldProps(`workspaces.${workspaceIndex}.name`)}
+            placeholder="Workspace Name"
+            disabled={formik?.isSubmitting}
+          />
+          <InputError
+            // @ts-ignore
+            error={formik?.errors?.workspaces?.[workspaceIndex]?.name}
+            touched={formik?.touched?.workspaces?.[workspaceIndex]?.name}
+          />
+        </div>
+        <div>
+          <InputSelect
+            {...formik.getFieldProps(`workspaces.${workspaceIndex}.distro`)}
+            placeholder="Workspace Distro"
+          >
+            <Fragment>
+              {!formik?.values?.workspaces[workspaceIndex]?.distro && (
+                <option value=""></option>
+              )}
+              <option value="humble">Humble</option>
+              <option value="foxy">Foxy</option>
+              <option value="galactic">Galactic</option>
+            </Fragment>
+          </InputSelect>
+        </div>
+        <div className="flex flex-col gap-3 border border-layer-light-100 p-5 rounded !shadow">
+          <span className="mx-auto text-[0.75rem] font-medium">
+            Workspace Repositories
+          </span>
+          <span className="w-full h-[2px] bg-primary" />
+
+          {formik.values.workspaces[workspaceIndex]?.repositories?.map(
+            (repository: any, repositoryIndex: number) => (
+              <CreateRobotFormRepositoryItem
+                key={repositoryIndex}
+                activeRepository={activeRepository}
+                setActiveRepository={setActiveRepository}
+                formik={formik}
+                workspaceIndex={workspaceIndex}
+                repositoryIndex={repositoryIndex}
+                repository={repository}
+              />
+            )
+          )}
+          <BsPlusCircle
+            onClick={() => handleAddRepository(workspaceIndex)}
+            size={22}
+            className="mx-auto text-layer-secondary-700 hover:scale-90 transition-all duration-500 cursor-pointer"
+          />
+        </div>
+        <span
+          onClick={() => {
+            setActiveWorkspace(-1);
+            handleRemoveWorkspace(workspaceIndex);
+          }}
+          className="text-[0.66rem] text-red-500 cursor-pointer mx-auto hover:underline"
+        >
+          Delete {workspace?.name ? workspace.name : `this`} Workspace
+        </span>
+      </div>
+    </Accordion>
+  );
+}
