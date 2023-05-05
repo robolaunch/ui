eca71385a40e9f387a9bc3db8a34623adb592d60
gokhangunduz <mail@gokhangunduz.com.tr>
2023-05-05 16:51:10 +0300

    feat(githubInterfaces): :tada: Add github interfaces

---
+++ src/interfaces/githubInterfaces.ts
@@ -0,0 +1,9 @@
+export interface IGithubToken {
+  access_token: string;
+  exp: number;
+  expires_in: number;
+  refresh_token: string;
+  refresh_token_expires_in: number;
+  scope: string;
+  token_type: string;
+}
