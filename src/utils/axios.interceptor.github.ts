ec36b57b6dc6b796a7f995ecb97b56211d8748f9
gokhangunduz <mail@gokhangunduz.com.tr>
2023-05-05 16:48:19 +0300

    refactor(axios.interceptor.github): :bug: Add axios interceptor for github

---
+++ src/utils/axios.interceptor.github.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const axiosInterceptorGithub: any = axios.create({});
+
+axiosInterceptorGithub.interceptors.request.use(async (req: any) => {
+  req.headers.Authorization = `Bearer ${
+    JSON.parse(localStorage.getItem("githubTokens") as any)?.access_token
+  }`;
+  req.headers.Accept = `application/vnd.github+json`;
+  req.headers["Content-Type"] = `application/json`;
+  return req;
+});
+
+axiosInterceptorGithub.interceptors.response.use(async (res: any) => {
+  return res;
+});
+
+export default axiosInterceptorGithub;
