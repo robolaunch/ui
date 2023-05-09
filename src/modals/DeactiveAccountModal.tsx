526dfdecd1f9db721101b0de012332b6abcdfbc6
gokhangunduz <mail@gokhangunduz.com.tr>
2023-05-09 13:24:35 +0300

    feat(DeactiveAccountModal): :tada: Add deactive account modal

---
+++ src/modals/DeactiveAccountModal.tsx
@@ -0,0 +1,74 @@
+import { useFormik } from "formik";
+import { Dialog } from "primereact/dialog";
+import React, { ReactElement } from "react";
+import InputText from "../components/InputText/InputText";
+import InputError from "../components/InputError/InputError";
+import Button from "../components/Button/Button";
+import { useKeycloak } from "@react-keycloak/web";
+import * as Yup from "yup";
+
+interface IDeactiveAccountModal {
+  visibleModal: boolean;
+  handleCloseModal: () => void;
+}
+
+export default function DeactiveAccountModal({
+  visibleModal,
+  handleCloseModal,
+}: IDeactiveAccountModal): ReactElement {
+  const { keycloak } = useKeycloak();
+
+  const formik = useFormik({
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .required("Required")
+        .equals(
+          [keycloak?.tokenParsed?.preferred_username],
+          "Username is not correct"
+        ),
+    }),
+    initialValues: {
+      username: "",
+    },
+    onSubmit: (values: any) => {},
+  });
+
+  return (
+    <Dialog
+      header="DeactiveAccountModal"
+      visible={visibleModal}
+      className="w-[30vw]"
+      onHide={() => handleCloseModal()}
+    >
+      <form
+        onSubmit={formik.handleSubmit}
+        className="w-full flex flex-col gap-8"
+      >
+        <p className="text-sm">
+          Please enter your username (
+          <b>{keycloak?.tokenParsed?.preferred_username}</b>) to confirm that
+          you want to deactivate your account.
+        </p>
+        <div className="w-full">
+          <InputText
+            {...formik.getFieldProps("username")}
+            placeholder="Username"
+            value={formik.values.username}
+          />
+          <InputError
+            touched={formik.touched.username}
+            error={formik.errors.username}
+          />
+        </div>
+        <div className="flex justify-end items-center gap-4">
+          <Button
+            className="!w-56 !h-11"
+            type="submit"
+            text="Deactive Account"
+            disabled={formik.isSubmitting || !formik.isValid}
+          />
+        </div>
+      </form>
+    </Dialog>
+  );
+}
