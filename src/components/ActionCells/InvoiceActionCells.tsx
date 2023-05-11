87b6416ce693474b8b2cfd5bde07d1473fb2611c
gokhangunduz <mail@gokhangunduz.com.tr>
2023-05-11 17:04:14 +0300

    refactor(InvoiveActionCells and UserActionCells): :tada: Update actions cells

--- src/components/ActionCells/InvoiceActionCells.tsx
+++ src/components/ActionCells/InvoiceActionCells.tsx
@@ -1,8 +1,9 @@
-import React, { ReactElement } from "react";
+import React, { Fragment, ReactElement, useState } from "react";
 import Button from "../Button/Button";
 import { MdPayment } from "react-icons/md";
 import { TbCloudDownload } from "react-icons/tb";
 import { AiOutlineEye } from "react-icons/ai";
+import InvoiceUtilizationModal from "../../modals/InvoiceUtilizationModal";
 
 interface IInvoiceActionCells {
   rowData: any;
@@ -11,31 +12,45 @@ interface IInvoiceActionCells {
 export default function InvoiceActionCells({
   rowData,
 }: IInvoiceActionCells): ReactElement {
+  const [isOpenInvoiceUtilizationModal, setIsOpenInvoiceUtilizationModal] =
+    useState<boolean>(false);
+
   return (
-    <div className="flex gap-3 justify-end">
-      <Button
-        className="!w-8 !h-8 !bg-transparent !border !border-layer-primary-500
+    <Fragment>
+      <div className="flex gap-3 justify-end">
+        <Button
+          className="!w-8 !h-8 !bg-transparent !border !border-layer-primary-500
                 disabled:!border-layer-light-300 disabled:!text-layer-light-300 disabled:cursor-not-allowed
                 "
-        text={
-          <MdPayment
-            className={`${
-              rowData?.status
-                ? "!text-layer-light-300"
-                : "!text-layer-primary-500"
-            } `}
-          />
-        }
-        disabled={rowData?.status}
-      />
-      <Button
-        className="!w-8 !h-8 !bg-transparent !border !border-layer-secondary-600 !ring-layer-secondary-200"
-        text={<TbCloudDownload className="text-layer-secondary-600" />}
-      />
-      <Button
-        className="!w-8 !h-8 !bg-transparent !border !border-red-600 !ring-red-200"
-        text={<AiOutlineEye className="text-red-600" />}
-      />
-    </div>
+          text={
+            <MdPayment
+              className={`${
+                rowData?.status
+                  ? "!text-layer-light-300"
+                  : "!text-layer-primary-500"
+              } `}
+            />
+          }
+          disabled={rowData?.status}
+        />
+        <Button
+          className="!w-8 !h-8 !bg-transparent !border !border-layer-secondary-600 !ring-layer-secondary-200"
+          text={<TbCloudDownload className="text-layer-secondary-600" />}
+        />
+        <Button
+          onClick={() => setIsOpenInvoiceUtilizationModal(true)}
+          className="!w-8 !h-8 !bg-transparent !border !border-red-600 !ring-red-200"
+          text={<AiOutlineEye className="text-red-600" />}
+        />
+      </div>
+      {isOpenInvoiceUtilizationModal && (
+        <InvoiceUtilizationModal
+          visibleModal={isOpenInvoiceUtilizationModal}
+          handleCloseModal={() =>
+            setIsOpenInvoiceUtilizationModal(!isOpenInvoiceUtilizationModal)
+          }
+        />
+      )}
+    </Fragment>
   );
 }
--- src/components/ActionCells/UserActionCells.tsx
+++ src/components/ActionCells/UserActionCells.tsx
@@ -1,14 +1,8 @@
-import React, { Fragment, ReactElement, useContext, useState } from "react";
-import { Dialog } from "primereact/dialog";
-import { useFormik } from "formik";
-import InputError from "../InputError/InputError";
-import InputText from "../InputText/InputText";
+import React, { Fragment, ReactElement, useState } from "react";
 import Button from "../Button/Button";
 import { BiTrash, BiPencil } from "react-icons/bi";
-import { deleteUserFromOrganizationSchema } from "../../validations/UsersValidations";
-import { ApiContext } from "../../contexts/ApiContext";
-import { Api } from "../../types/types";
-import toastApiNotifaction from "../../tools/toastApiNotifaction";
+import DeleteMemberFromOrganizationModal from "../../modals/DeleteMemberFromOrganizationModal";
+import ChangeRoleModal from "../../modals/ChangeRoleModal";
 interface IUserActionCells {
   data: any;
   onClickSee: any;
@@ -21,80 +15,9 @@ export default function UserActionCells({
   activePage,
   handleRefresh,
 }: IUserActionCells): ReactElement {
-  const [visibleChangeRoleModal, setVisibleChangeRoleModal] = useState(false);
-  const [visibleDelete, setVisibleDelete] = useState(false);
-  const [changeRoleSubmitting, setChangeRoleSubmitting] = useState(false);
-  const { api }: Api = useContext(ApiContext);
-
-  function handleChangeRole() {
-    setChangeRoleSubmitting(true);
-    if (activePage?.page === "organizationAdmins") {
-      api
-        .moveAdminAsUserFromOrganization({
-          name: "",
-          organizationId: activePage?.selectedOrganization?.organizationId,
-          invitedUserId: data?.userId,
-        })
-        .then((res: any) => toastApiNotifaction({ res: res }));
-    } else {
-      api
-        .moveUserAsAdminToOrganization({
-          name: "",
-          organizationId: activePage?.selectedOrganization?.organizationId,
-          invitedUserId: data?.userId,
-        })
-        .then((res: any) => toastApiNotifaction({ res: res }));
-    }
-    handleRefresh && handleRefresh();
-    setChangeRoleSubmitting(false);
-    setVisibleChangeRoleModal(false);
-  }
-
-  const deleteFormik = useFormik({
-    initialValues: {
-      username: data?.username,
-    },
-    validationSchema: deleteUserFromOrganizationSchema(data?.username),
-    onSubmit: (values) => {
-      deleteFormik.setSubmitting(true);
-
-      switch (activePage?.page) {
-        case "organizationAdmins":
-          api
-            .deleteAdminFromOrganization({
-              name: "",
-              organizationId: activePage?.selectedOrganization?.organizationId,
-              invitedUserId: data?.userId,
-            })
-            .then((res: any) => toastApiNotifaction({ res: res }));
-          break;
-        case "organizationUsers":
-          api
-            .deleteUserFromOrganization({
-              name: "",
-
-              organizationId: activePage?.selectedOrganization?.organizationId,
-              invitedUserId: data?.userId,
-            })
-            .then((res: any) => toastApiNotifaction({ res: res }));
-          break;
-        case "organizationGuests":
-          api
-            .deleteGuestFromOrganization({
-              name: "",
-              organizationId: activePage?.selectedOrganization?.organizationId,
-              invitedUserId: data?.userId,
-            })
-            .then((res: any) => toastApiNotifaction({ res: res }));
-          break;
-      }
-
-      deleteFormik.resetForm();
-      handleRefresh && handleRefresh();
-      deleteFormik.setSubmitting(false);
-      setVisibleDelete(false);
-    },
-  });
+  const [visibleChangeRoleModal, setVisibleChangeRoleModal] =
+    useState<boolean>(false);
+  const [visibleDeleteModal, setVisibleDeleteModal] = useState<boolean>(false);
 
   return (
     <Fragment>
@@ -107,80 +30,27 @@ export default function UserActionCells({
         <Button
           className="!w-8 !h-8 !bg-transparent !border !border-red-600"
           text={<BiTrash className="text-red-600" />}
-          onClick={() => setVisibleDelete(true)}
+          onClick={() => setVisibleDeleteModal(true)}
         />
       </div>
-      <Dialog
-        header="Change Role"
-        visible={visibleChangeRoleModal}
-        className="w-[40vw]"
-        onHide={() => setVisibleChangeRoleModal(false)}
-      >
-        <div className="w-full flex flex-col gap-8">
-          <p className="text-sm">
-            This user now is{" "}
-            <span className="font-semibold">
-              "{activePage?.page === "organizationAdmins" ? "Admin" : "Member"}"
-            </span>
-            . Are you sure you want to change role to{" "}
-            <span className="font-semibold">
-              {activePage?.page === "organizationAdmins" ? "Member" : "Admin"}
-            </span>
-            ?
-          </p>
-          <div className="flex justify-end items-center gap-4">
-            <Button
-              className="!w-44 !h-11"
-              type="submit"
-              text={`Change Role to ${
-                activePage?.page === "organizationAdmins" ? "Member" : "Admin"
-              }`}
-              disabled={changeRoleSubmitting}
-              onClick={() => handleChangeRole()}
-            />
-          </div>
-        </div>
-      </Dialog>
-      <Dialog
-        header="Delete User from Organization"
-        visible={visibleDelete}
-        className="w-[40vw]"
-        onHide={() => setVisibleDelete(false)}
-      >
-        <form
-          onSubmit={deleteFormik.handleSubmit}
-          className="w-full flex flex-col gap-8"
-        >
-          <p className="text-sm">
-            Are you sure you want to delete this user from the{" "}
-            {activePage?.page === "organizationAdmins"
-              ? "organization admins"
-              : activePage?.page === "organizationUsers"
-              ? "organization users"
-              : "organization guests"}
-            ?
-          </p>
-          <div className="w-full">
-            <InputText
-              {...deleteFormik.getFieldProps("username")}
-              placeholder="Username"
-              disabled={deleteFormik.isSubmitting}
-            />
-            <InputError
-              touched={deleteFormik.touched.username}
-              error={deleteFormik.errors.username}
-            />
-          </div>
-          <div className="flex justify-end items-center gap-4">
-            <Button
-              className="!w-56 !h-11"
-              type="submit"
-              text="Delete User from Organization"
-              disabled={deleteFormik.isSubmitting || !deleteFormik.isValid}
-            />
-          </div>
-        </form>
-      </Dialog>
+      {visibleChangeRoleModal && (
+        <ChangeRoleModal
+          activePage={activePage}
+          data={data}
+          handleCloseModal={() => setVisibleChangeRoleModal(false)}
+          handleRefresh={handleRefresh}
+          visibleModal={visibleChangeRoleModal}
+        />
+      )}
+      {visibleDeleteModal && (
+        <DeleteMemberFromOrganizationModal
+          data={data}
+          handleCloseModal={() => setVisibleDeleteModal(false)}
+          handleRefresh={handleRefresh}
+          visibleModal={visibleDeleteModal}
+          activePage={activePage}
+        />
+      )}
     </Fragment>
   );
 }
