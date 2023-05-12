7b2d4a7f0aea37dd20234b92f910388013695034
gokhangunduz <mail@gokhangunduz.com.tr>
2023-05-12 12:55:45 +0300

    refactor(Breadcrumb): :boom: Add new icons breadcrumb

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from "react";
 import stringCapitalization from "../../helpers/stringCapitalization";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import useBreadcrumbs from "use-react-router-breadcrumbs";
 import {
   RiBillFill,
@@ -12,6 +12,7 @@ import {
 
 export default function Breadcrumb(): ReactElement {
   const breadcrumbs = useBreadcrumbs();
+  const params = useParams();
 
   return (
     <ul className="flex gap-2 text-xs font-medium">
@@ -19,21 +20,70 @@ export default function Breadcrumb(): ReactElement {
         return (
           <li className="flex items-center justify-center gap-2" key={index}>
             <Link
-              className="flex items-center justify-center gap-0.5"
+              className="flex items-center justify-center gap-1"
               to={item?.key}
             >
               {(() => {
-                switch (item?.breadcrumb?.props?.children) {
-                  case "Home":
+                if (
+                  params?.organizationName ||
+                  params?.roboticsCloudName ||
+                  params?.fleetName ||
+                  params?.robotName
+                ) {
+                  if (index === 0) {
                     return <RiDashboardFill size={14} />;
-                  case "Profile":
-                    return <RiUser3Fill className="mt-0.5" size={13} />;
-                  case "Marketplace":
-                    return <RiStore2Fill className="mt-0.5" size={13} />;
-                  case "User role management":
-                    return <RiUserSettingsFill className="mt-0.5" size={13} />;
-                  case "Billing":
-                    return <RiBillFill className="mt-0.5" size={13} />;
+                  }
+                  if (params?.organizationName && index === 1) {
+                    return (
+                      <img
+                        className="w-3"
+                        src="/svg/sidebar/organization/organization-gray.svg"
+                        alt="rc"
+                      />
+                    );
+                  }
+                  if (params?.roboticsCloudName && index === 2) {
+                    return (
+                      <img
+                        className="w-4"
+                        src="/svg/sidebar/roboticscloud/roboticscloud-gray.svg"
+                        alt="rc"
+                      />
+                    );
+                  }
+                  if (params?.fleetName && index === 3) {
+                    return (
+                      <img
+                        className="w-3"
+                        src="/svg/sidebar/fleet/fleet-gray.svg"
+                        alt="rc"
+                      />
+                    );
+                  }
+                  if (params?.robotName && index === 4) {
+                    return (
+                      <img
+                        className="w-3"
+                        src="/svg/sidebar/robot/robot-gray.svg"
+                        alt="rc"
+                      />
+                    );
+                  }
+                } else {
+                  switch (item?.breadcrumb?.props?.children) {
+                    case "Home":
+                      return <RiDashboardFill size={14} />;
+                    case "Profile":
+                      return <RiUser3Fill className="mt-0.5" size={13} />;
+                    case "Marketplace":
+                      return <RiStore2Fill className="mt-0.5" size={13} />;
+                    case "User role management":
+                      return (
+                        <RiUserSettingsFill className="mt-0.5" size={13} />
+                      );
+                    case "Billing":
+                      return <RiBillFill className="mt-0.5" size={13} />;
+                  }
                 }
               })()}
               <span className="mt-0.5">
