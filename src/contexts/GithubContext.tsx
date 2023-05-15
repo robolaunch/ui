b1a3a2a7cc0303c0a63140e4fe1a035ca4ed309c
gokhangunduz <mail@gokhangunduz.com.tr>
2023-05-15 17:49:13 +0300

    refactor(*): :book: Update context flow and remove old apis

--- src/contexts/GithubContext.tsx
+++ src/contexts/GithubContext.tsx
@@ -1,14 +1,20 @@
 import React, { createContext, useEffect, useState } from "react";
 import queryString from "query-string";
-import axios from "axios";
 import { IGithubToken } from "../interfaces/githubInterfaces";
 import { useKeycloak } from "@react-keycloak/web";
+import { toast } from "sonner";
+import { useAppDispatch } from "../hooks/redux";
+import {
+  getGithubAccessTokenwithCode,
+  getGithubAccessTokenwithRefreshToken,
+} from "../resources/GithubSlice";
 export const GithubContext: any = createContext<any>(null);
 
 // eslint-disable-next-line
 export default ({ children }: any) => {
+  const dispatch = useAppDispatch();
   const { keycloak } = useKeycloak();
-
+  const queryParams = queryString.parse(window.location.search);
   const [githubToken, setGithubToken] = useState<IGithubToken | null>(() => {
     if (
       JSON.parse(localStorage.getItem("githubTokens") as any) &&
@@ -23,96 +29,76 @@ export default ({ children }: any) => {
     }
   });
 
-  const queryParams = queryString.parse(window.location.search);
-
   useEffect(() => {
-    if (!githubToken && keycloak?.tokenParsed?.githubApp) {
-      if (queryParams?.code) {
-        getGithubAccessTokenwithCode();
-      } else {
-        window.location.href = `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_APP_CLIENT_ID}`;
-      }
-    } else {
-      // window.history.pushState({}, "", "/");
+    if (keycloak?.tokenParsed?.githubApp && !githubToken && queryParams?.code) {
+      getGithubAppToken();
       window.history.replaceState({}, document.title, window.location.pathname);
+    } else if (keycloak?.tokenParsed?.githubApp && !githubToken) {
+      getGithubAppCode();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
+    localStorage.setItem("githubTokens", JSON.stringify(githubToken));
     const tokenExpriationTimer =
       githubToken &&
       keycloak?.tokenParsed?.githubApp &&
       setTimeout(() => {
-        getGithubAccessTokenwithRefreshToken();
+        handleRefreshToken();
       }, (githubToken?.exp - Math.floor(Date.now() / 1000) - 900) * 1000);
     return () => clearTimeout(tokenExpriationTimer || 0);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [githubToken]);
 
-  useEffect(() => {
-    console.log(githubToken);
-    localStorage.setItem("githubTokens", JSON.stringify(githubToken));
-  }, [githubToken]);
-
-  function getGithubAccessTokenwithCode() {
-    axios
-      .post(
-        "http://localhost:8081/getGithubAccessTokenwithCode",
-        {
-          code: queryParams.code,
-          githubUserId: keycloak?.tokenParsed?.githubApp,
-        },
-        {
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-        }
-      )
-      .then((response) => {
-        if (!response?.data?.error) {
-          console.log("gettoken", response);
-          setGithubToken({
-            exp: response.data.expires_in + Math.floor(Date.now() / 1000),
-            ...response.data,
-          });
-          console.log("githubTokenReceived");
-        }
-      })
-      .catch((error) => {
-        setGithubToken(null);
-      });
+  function getGithubAppCode() {
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_APP_CLIENT_ID}`;
   }
 
-  function getGithubAccessTokenwithRefreshToken() {
-    axios
-      .post(
-        "http://localhost:8081/getGithubAccessTokenwithRefreshToken",
-        {
-          refresh_token: githubToken?.refresh_token,
-        },
-        {
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-        }
-      )
-      .then((response) => {
-        if (!response?.data?.error) {
-          setGithubToken({
-            exp: response.data.expires_in + Math.floor(Date.now() / 1000),
-            ...response.data,
-          });
+  function getGithubAppToken() {
+    dispatch(
+      getGithubAccessTokenwithCode({
+        code: queryParams.code,
+        githubUserId: keycloak?.tokenParsed?.githubApp,
+      })
+    ).then((response: any) => {
+      console.log(response);
+      if (!response.payload?.data?.error) {
+        setGithubToken({
+          exp: response.payload.data.expires_in + Math.floor(Date.now() / 1000),
+          ...response.payload.data,
+        });
+        console.log("githubTokenReceived");
+      } else {
+        toast.error(
+          "Github App Auth failed. Please sign in your's github account."
+        );
+        setGithubToken(null);
+      }
+    });
+  }
 
+  function handleRefreshToken() {
+    dispatch(
+      getGithubAccessTokenwithRefreshToken({
+        refresh_token: githubToken?.refresh_token,
+      })
+    )
+      .then((response: any) => {
+        if (!response?.payload?.data?.error) {
+          setGithubToken({
+            exp:
+              response?.payload?.data.expires_in +
+              Math.floor(Date.now() / 1000),
+            ...response?.payload?.data,
+          });
           console.log("githubTokenRenewed");
         } else {
-          window.location.href = `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_APP_CLIENT_ID}`;
+          getGithubAppCode();
         }
       })
       .catch((error) => {
-        window.location.href = `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_APP_CLIENT_ID}`;
+        getGithubAppCode();
       });
   }
 
