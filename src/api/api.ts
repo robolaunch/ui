7f322d25f4ec0e01b92145bc3c41ce5de858d0b6
gokhangunduz <mail@gokhangunduz.com.tr>
2023-05-15 14:09:35 +0300

    feat(api.ts): :tada: Add new openapi clients

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,13 @@
 import axiosInterceptor from "../utils/axios.interceptor";
-import { Configuration, OrganizationRepositoryImplApi } from "./openapi";
+import {
+  Configuration,
+  OrganizationRepositoryImplApi,
+  AwsApi,
+  RobotApi,
+  KubernetesApi,
+  RobotBuildManagerApi,
+  CreateInstanceApi,
+} from "./openapi";
 
 const apiConfig: Configuration = new Configuration({
   basePath: process.env.REACT_APP_BACKEND_URL,
@@ -11,3 +19,34 @@ export const organizationApi: OrganizationRepositoryImplApi =
     process.env.REACT_APP_BACKEND_URL,
     axiosInterceptor
   );
+
+export const awsApi: AwsApi = new AwsApi(
+  apiConfig,
+  process.env.REACT_APP_BACKEND_URL,
+  axiosInterceptor
+);
+
+export const robotApi: RobotApi = new RobotApi(
+  apiConfig,
+  process.env.REACT_APP_BACKEND_URL,
+  axiosInterceptor
+);
+
+export const kubernetesApi: KubernetesApi = new KubernetesApi(
+  apiConfig,
+  process.env.REACT_APP_BACKEND_URL,
+  axiosInterceptor
+);
+
+export const robotBuildManagerApi: RobotBuildManagerApi =
+  new RobotBuildManagerApi(
+    apiConfig,
+    process.env.REACT_APP_BACKEND_URL,
+    axiosInterceptor
+  );
+
+export const createInstanceApi: CreateInstanceApi = new CreateInstanceApi(
+  apiConfig,
+  process.env.REACT_APP_BACKEND_URL,
+  axiosInterceptor
+);
