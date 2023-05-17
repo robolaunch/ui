8ef49c4741932aafbf259ec9c3da0c214be19d8e
gokhangunduz <mail@gokhangunduz.com.tr>
2023-05-17 18:21:49 +0300

    reafactor(RoboticsCloudsValidations): :tada: Update create robotics cloud validations

--- src/validations/RoboticsCloudsValidations.ts
+++ src/validations/RoboticsCloudsValidations.ts
@@ -1,10 +1,7 @@
 import * as Yup from "yup";
 
 export const createRoboticsCloudSchema = Yup.object().shape({
-  provider: Yup.string().required("Provider is required"),
-  region: Yup.string().required("Region is required"),
-  instanceType: Yup.string().required("Instance type is required"),
-  name: Yup.string()
+  roboticsCloudName: Yup.string()
     .min(2, "Min 2 Character")
     .max(32, "Max 32 Character")
     .matches(/^\w[a-zA-Z@#0-9.]*$/, "Only english symbols and numbers")
