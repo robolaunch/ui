d44a0fe05c249de7b6341e737a34beb5a335a874
gokhangunduz <mail@gokhangunduz.com.tr>
2023-05-18 14:00:13 +0300

    feat(InstanceActionCells): :bug: Add terminate case in instance action cells

--- src/components/ActionCells/InstanceActionCells.tsx
+++ src/components/ActionCells/InstanceActionCells.tsx
@@ -2,6 +2,7 @@ import React, { Fragment, ReactElement, useState } from "react";
 import Button from "../Button/Button";
 import { BiTrash, BiStopCircle, BiPlayCircle } from "react-icons/bi";
 import ChangeStateInstaceModal from "../../modals/ChangeStateInstaceModal";
+import TerminateInstanceModal from "../../modals/TerminateInstanceModal";
 
 interface IInstanceActionCells {
   data: any;
@@ -14,32 +15,52 @@ export default function InstanceActionCells({
 }: IInstanceActionCells): ReactElement {
   const [isChangeStateModalVisible, setIsChangeStateModalVisible] =
     useState<boolean>(false);
+  const [isTerminateModalVisible, setIsTerminateModalVisible] =
+    useState<boolean>(false);
 
   return (
     <Fragment>
       <div className="card flex gap-4 float-right">
-        {data?.state === "running" ? (
-          <Button
-            className="!w-8 !h-8 !bg-transparent !border !border-layer-primary-500"
-            text={<BiStopCircle size={20} className="text-layer-primary-500" />}
-            onClick={() => setIsChangeStateModalVisible(true)}
-          />
-        ) : data?.state === "stopped" ? (
-          <Button
-            className="!w-8 !h-8 !bg-transparent !border !border-layer-primary-500"
-            text={<BiPlayCircle size={20} className="text-layer-primary-500" />}
-            onClick={() => setIsChangeStateModalVisible(true)}
-          />
-        ) : (
-          <Button
-            className="!w-8 !h-8 !bg-transparent !border !border-layer-dark-100"
-            text={<img src="/svg/general/loading.svg" alt="loading" />}
-            disabled
-          />
-        )}
         <Button
-          className="!w-8 !h-8 !bg-transparent !border !border-red-600"
-          text={<BiTrash className="text-red-600" />}
+          className={`!w-8 !h-8 !bg-transparent !border ${
+            data?.state === "running" || data?.state === "stopped"
+              ? "!border-layer-primary-500"
+              : "!border-layer-dark-100"
+          }`}
+          text={
+            data?.state === "running" ? (
+              <BiStopCircle size={20} className="text-layer-primary-500" />
+            ) : data?.state === "stopped" ? (
+              <BiPlayCircle size={20} className="text-layer-primary-500" />
+            ) : (
+              <img src="/svg/general/loading.svg" alt="loading" />
+            )
+          }
+          disabled={
+            data?.state === "running" || data?.state === "stopped"
+              ? false
+              : true
+          }
+          onClick={
+            data?.state === "running" || data?.state === "stopped"
+              ? () => setIsChangeStateModalVisible(true)
+              : () => {}
+          }
+        />
+        <Button
+          className={`!w-8 !h-8 !bg-transparent !border  ${
+            data?.state === "running" || data?.state === "stopped"
+              ? "!border-red-600"
+              : "!border-layer-dark-100"
+          }`}
+          text={
+            data?.state === "running" || data?.state === "stopped" ? (
+              <BiTrash className="text-red-600" />
+            ) : (
+              <img src="/svg/general/loading.svg" alt="loading" />
+            )
+          }
+          onClick={() => setIsTerminateModalVisible(true)}
         />
       </div>
       {isChangeStateModalVisible && (
@@ -48,6 +69,14 @@ export default function InstanceActionCells({
           reload={reload}
           handleCloseModal={() => setIsChangeStateModalVisible(false)}
           visibleModal={isChangeStateModalVisible}
+        />
+      )}
+      {isTerminateModalVisible && (
+        <TerminateInstanceModal
+          data={data}
+          reload={reload}
+          handleCloseModal={() => setIsTerminateModalVisible(false)}
+          visibleModal={isTerminateModalVisible}
         />
       )}
     </Fragment>
