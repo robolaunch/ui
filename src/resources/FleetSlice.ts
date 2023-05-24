ede8923b8be4f2614b3b23d91413caff17e38c47
gokhangunduz <mail@gokhangunduz.com.tr>
2023-05-24 17:56:42 +0300

    refactor(FleetSlice): :boom: Add new endpoints

--- src/resources/FleetSlice.ts
+++ src/resources/FleetSlice.ts
@@ -1,9 +1,218 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { kubernetesApi } from "../api/api";
+import { toast } from "sonner";
+
+export const createFederatedFleet = createAsyncThunk(
+  "instance/createFederatedFleet",
+  async (values: any) => {
+    const response = await kubernetesApi.createFederatedFleet({
+      name: values?.name,
+      organizationId: values?.organizationId,
+      roboticsClouds: [
+        {
+          name: values?.roboticsCloudName,
+          cloudInstances: [
+            {
+              instanceId: values?.instanceId,
+              region: values?.region,
+              robolaunchFederatedFleets: [
+                { name: values?.robolaunchFederatedFleetsName },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+    return response.data;
+  }
+);
+
+export const getFederatedFleets = createAsyncThunk(
+  "instance/getFederatedFleets",
+  async (values: any) => {
+    const response = await kubernetesApi.getFederatedFleets({
+      name: values?.name,
+      organizationId: values?.organizationId,
+      roboticsClouds: [
+        {
+          name: values?.roboticsCloudName,
+          cloudInstances: [
+            { instanceId: values?.instanceId, region: values?.region },
+          ],
+        },
+      ],
+    });
+    return response.data;
+  }
+);
+
+export const getFederatedFleetStatus = createAsyncThunk(
+  "instance/getFederatedFleetStatus",
+  async (values: any) => {
+    const response = await kubernetesApi.getFederatedFleetStatus({
+      name: values.name,
+      organizationId: "b3c47588-0fc1-4753-9faa-8fda72503b5d",
+      roboticsClouds: [
+        {
+          name: "robotics-cloud-04",
+          cloudInstances: [
+            {
+              instanceId: "i-045fbc86880c38054",
+              region: "eu-central-1",
+              robolaunchFederatedFleets: [{ name: "test-fleet" }],
+            },
+          ],
+        },
+      ],
+    });
+    return response.data;
+  }
+);
+
+export const deleteFederatedFleet = createAsyncThunk(
+  "instance/deleteFederatedFleet",
+  async (values: any) => {
+    const response = await kubernetesApi.deleteFederatedFleet({
+      name: values?.name,
+      organizationId: values?.organizationId,
+      roboticsClouds: [
+        {
+          name: values?.roboticsCloudName,
+          cloudInstances: [
+            {
+              instanceId: values?.instanceId,
+              region: values?.region,
+              robolaunchFederatedFleets: [
+                { name: values?.robolaunchFederatedFleetsName },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+    return response.data;
+  }
+);
+
+export const addPhysicalInstance = createAsyncThunk(
+  "instance/addPhysicalInstance",
+  async (values: any) => {
+    const response = await kubernetesApi.addPhysicalInstance({
+      name: values?.name,
+      organizationId: values?.organizationId,
+      roboticsClouds: [
+        {
+          name: values?.roboticsCloudName,
+          cloudInstances: [
+            {
+              instanceId: values?.instanceId,
+              region: values?.region,
+              robolaunchPhysicalInstances: [
+                { name: values?.robolaunchPhysicalInstancesName },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+    return response.data;
+  }
+);
+
+export const getPhysicalInstances = createAsyncThunk(
+  "instance/getPhysicalInstances",
+  async (values: any) => {
+    const response = await kubernetesApi.getPhysicalInstances({
+      name: values?.name,
+      organizationId: values?.organizationId,
+      roboticsClouds: [
+        {
+          name: values?.roboticsCloudName,
+          cloudInstances: [
+            { instanceId: values?.instanceId, region: values?.region },
+          ],
+        },
+      ],
+    });
+    return response.data;
+  }
+);
+
+export const addPhysicalInstanceToFleet = createAsyncThunk(
+  "instance/addPhysicalInstanceToFleet",
+  async (values: any) => {
+    const response = await kubernetesApi.addPhysicalInstanceToFleet({
+      name: values?.name,
+      organizationId: values?.organizationId,
+      roboticsClouds: [
+        {
+          name: values?.roboticsCloudName,
+          cloudInstances: [
+            {
+              instanceId: values?.instanceId,
+              region: values?.region,
+              robolaunchPhysicalInstances: [
+                { name: values?.robolaunchPhysicalInstancesName },
+              ],
+              robolaunchFederatedFleets: [
+                { name: values?.robolaunchFederatedFleetsName },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+    return response.data;
+  }
+);
 
 export const fleetSlice = createSlice({
   name: "fleet",
   initialState: {},
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createFederatedFleet.fulfilled, (_, action: any) => {
+        if (!action?.payload?.success) {
+          toast.error(action?.payload?.message);
+        } else {
+          toast.success(action?.payload?.message);
+        }
+      })
+      .addCase(createFederatedFleet.rejected, (_) => {
+        toast.error("Something went wrong");
+      })
+      .addCase(deleteFederatedFleet.fulfilled, (_, action: any) => {
+        if (!action?.payload?.success) {
+          toast.error(action?.payload?.message);
+        } else {
+          toast.success(action?.payload?.message);
+        }
+      })
+      .addCase(deleteFederatedFleet.rejected, (_) => {
+        toast.error("Something went wrong");
+      })
+      .addCase(addPhysicalInstance.fulfilled, (_, action: any) => {
+        if (!action?.payload?.success) {
+          toast.error(action?.payload?.message);
+        } else {
+          toast.success(action?.payload?.message);
+        }
+      })
+      .addCase(addPhysicalInstance.rejected, (_) => {
+        toast.error("Something went wrong");
+      })
+      .addCase(addPhysicalInstanceToFleet.fulfilled, (_, action: any) => {
+        if (!action?.payload?.success) {
+          toast.error(action?.payload?.message);
+        } else {
+          toast.success(action?.payload?.message);
+        }
+      })
+      .addCase(addPhysicalInstanceToFleet.rejected, (_) => {
+        toast.error("Something went wrong");
+      });
+  },
 });
 
 export default fleetSlice.reducer;
