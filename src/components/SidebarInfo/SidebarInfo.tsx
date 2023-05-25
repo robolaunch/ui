746ffc95af8b085a7686113f4ccfaa0eead929ee
gokhangunduz <mail@gokhangunduz.com.tr>
2023-05-25 17:57:00 +0300

    refactor(SidebarInfo): :tada: Update type parameter logic

---
+++ src/components/SidebarInfo/SidebarInfo.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement } from "react";
+
+interface ISidebarInfo {
+  text: string;
+}
+
+export default function SidebarInfo({ text }: ISidebarInfo): ReactElement {
+  return (
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <div className="text-base font-bold text-layer-dark-100 text-center">
+        {text}
+      </div>
+    </div>
+  );
+}
