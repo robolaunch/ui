e4779c99d11a28ddfeac194617b62d2ad8c37240
gokhangunduz <mail@gokhangunduz.com.tr>
2023-05-30 10:45:36 +0300

    feat(404): :rotating_light: Add 404 page and remove dynamic 404 page component

--- src/pages/Page404/Page404.tsx
+++ src/pages/Page404/Page404.tsx
@@ -1,24 +1,12 @@
 import React, { Fragment, ReactElement } from "react";
 import CardLayout from "../../layouts/CardLayout";
 import { Link } from "react-router-dom";
-import Button from "../Button/Button";
+import Button from "../../components/Button/Button";
 
-interface IPage404 {
-  description?: string;
-  className?: string;
-  buttonText?: string;
-  buttonURL?: any;
-}
-
-export default function Page404({
-  description,
-  className,
-  buttonText,
-  buttonURL,
-}: IPage404): ReactElement {
+export default function Page404(): ReactElement {
   return (
     <CardLayout
-      className={`flex flex-col justify-center items-center p-12 h-full gap-8 animate__animated animate__fadeIn transition-all duration-500 ${className}`}
+      className={`flex flex-col justify-center items-center p-12 h-full gap-8 animate__animated animate__fadeIn transition-all duration-500 `}
       style={{
         backgroundImage: `url("/images/abstract2-white.jpg")`,
         backgroundRepeat: "no-repeat",
@@ -28,11 +16,11 @@ export default function Page404({
     >
       <Fragment>
         <p className="font-semibold text-layer-dark-600">
-          {description ? description : "This page could not be found."}
+          This page could not be found.
         </p>
-        <Link to={buttonURL ? buttonURL : "/"}>
+        <Link to={"/"}>
           <Button
-            text={buttonText ? buttonText : "Redirect to Home Page"}
+            text={"Redirect to Home Page"}
             className="!w-fit !h-fit p-2.5 !border-2 !border-layer-primary-300 !text-xs !bg-transparent !font-semibold !text-layer-primary-600 hover:!scale-90 transition-all duration-500"
           />
         </Link>
