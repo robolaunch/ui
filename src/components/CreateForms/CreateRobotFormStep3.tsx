2372501d1d92b4aaeb8f65cb8af30953672fb77d
gokhangunduz <mail@gokhangunduz.com.tr>
2023-05-31 18:48:18 +0300

    feat(CreateRobotFormStep3): :tada: Add loading tab

--- src/components/CreateForms/CreateRobotFormStep3.tsx
+++ src/components/CreateForms/CreateRobotFormStep3.tsx
@@ -13,7 +13,7 @@ import { getFederatedRobot } from "../../resources/RobotSlice";
 export default function CreateRobotFormStep3(): ReactElement {
   const { robotData, setRobotData } = useCreateRobot();
   const dispatch = useAppDispatch();
-  const [responseRobot, setResponseRobot] = useState<any>(undefined);
+  const [responseRobot, setResponseRobot] = useState < any > undefined;
 
   const { handleCreateRobotPreviousStep, handleCreateRobotNextStep } =
     useSidebar();
@@ -38,32 +38,35 @@ export default function CreateRobotFormStep3(): ReactElement {
 
   const { selectedState } = useSidebar();
 
-  const formik: FormikProps<IRobotBuildSteps> = useFormik<IRobotBuildSteps>({
-    validationSchema: Yup.object().shape({
-      steps: Yup.array().of(
-        Yup.object().shape({
-          name: Yup.string()
-            .required("Step Name is required")
-            .test("unique-name", "This name is already taken", (value) => {
-              const temp = formik.values.steps.filter(
-                (item: any) => item.name === value && item
-              );
-              return temp.length > 1 ? false : true;
-            }),
-          workspace: Yup.string().required("Workspace is required"),
-          isScriptCode: Yup.boolean().required("Code Type is required"),
-          code: Yup.string().required("Code is required"),
-        })
-      ),
-    }),
-    initialValues: robotData?.step3,
-    onSubmit: (values: any) => {
-      formik.setSubmitting(true);
-      //
-      formik.setSubmitting(false);
-      handleCreateRobotNextStep();
-    },
-  });
+  const formik: FormikProps<IRobotBuildSteps> =
+    useFormik <
+    IRobotBuildSteps >
+    {
+      validationSchema: Yup.object().shape({
+        steps: Yup.array().of(
+          Yup.object().shape({
+            name: Yup.string()
+              .required("Step Name is required")
+              .test("unique-name", "This name is already taken", (value) => {
+                const temp = formik.values.steps.filter(
+                  (item: any) => item.name === value && item
+                );
+                return temp.length > 1 ? false : true;
+              }),
+            workspace: Yup.string().required("Workspace is required"),
+            isScriptCode: Yup.boolean().required("Code Type is required"),
+            code: Yup.string().required("Code is required"),
+          })
+        ),
+      }),
+      initialValues: robotData?.step3,
+      onSubmit: (values: any) => {
+        formik.setSubmitting(true);
+        //
+        formik.setSubmitting(false);
+        handleCreateRobotNextStep();
+      },
+    };
 
   useEffect(() => {
     setRobotData({
@@ -117,60 +120,101 @@ export default function CreateRobotFormStep3(): ReactElement {
     });
   }
 
+  useEffect(() => {
+    console.log(responseRobot);
+  }, [responseRobot]);
+
   return (
     <Fragment>
-      {!responseRobot ? (
-        <div className="w-full h-full flex flex-col gap-2 items-center justify-center">
-          <img
-            className="w-12 mx-auto pt-10"
-            src="/svg/general/loading.svg"
-            alt="Loading..."
-          />
-          <span className="text-sm text-layer-light-900">Robot State:</span>
-          <span className="text-sm text-layer-light-900">
-            Please wait while we create your robot. This may take a few minutes.
-          </span>
-        </div>
-      ) : (
-        <form
-          onSubmit={formik.handleSubmit}
-          className="flex flex-col gap-8 animate__animated animate__fadeIn"
-        >
-          <div className="flex flex-col gap-2">
-            {robotData?.step3?.steps?.map(
-              (buildStep: any, buildStepIndex: number) => {
-                return (
-                  <CreateRobotFormBuildStepItem
-                    key={buildStepIndex}
-                    formik={formik}
-                    buildStep={buildStep}
-                    buildStepIndex={buildStepIndex}
-                  />
-                );
-              }
-            )}
-          </div>
-          <BsPlusCircle
-            onClick={handleAddBuildStep}
-            size={22}
-            className="mx-auto text-layer-secondary-700 hover:scale-90 transition-all duration-500 cursor-pointer -mt-4"
-          />
-          <div className="flex gap-6">
-            <Button
-              onClick={handleCreateRobotPreviousStep}
-              type="reset"
-              className="!h-11 !bg-layer-light-50 !text-primary border border-primary hover:!bg-layer-primary-100 transition-all duration-500 text-xs"
-              text={`Previous Step`}
-            />
-            <Button
-              type="submit"
-              disabled={!formik?.isValid || formik.isSubmitting}
-              className="!h-11 text-xs"
-              text={`Create Robot`}
-            />
-          </div>
-        </form>
-      )}
+      {(() => {
+        if (
+          responseRobot?.robotClusters[0]?.robotStatus &&
+          responseRobot?.robotClusters[0]?.robotStatus === "EnvironmentReady" &&
+          responseRobot?.robotClusters[1]?.robotStatus &&
+          responseRobot?.robotClusters[1]?.robotStatus === "EnvironmentReady"
+        ) {
+          return (
+            <form
+              onSubmit={formik.handleSubmit}
+              className="flex flex-col gap-8 animate__animated animate__fadeIn"
+            >
+              <div className="flex flex-col gap-2">
+                {robotData?.step3?.steps?.map(
+                  (buildStep: any, buildStepIndex: number) => {
+                    return (
+                      <CreateRobotFormBuildStepItem
+                        key={buildStepIndex}
+                        formik={formik}
+                        buildStep={buildStep}
+                        buildStepIndex={buildStepIndex}
+                      />
+                    );
+                  }
+                )}
+              </div>
+              <BsPlusCircle
+                onClick={handleAddBuildStep}
+                size={22}
+                className="mx-auto text-layer-secondary-700 hover:scale-90 transition-all duration-500 cursor-pointer -mt-4"
+              />
+              <div className="flex gap-6">
+                <Button
+                  onClick={handleCreateRobotPreviousStep}
+                  type="reset"
+                  className="!h-11 !bg-layer-light-50 !text-primary border border-primary hover:!bg-layer-primary-100 transition-all duration-500 text-xs"
+                  text={`Previous Step`}
+                />
+                <Button
+                  type="submit"
+                  disabled={!formik?.isValid || formik.isSubmitting}
+                  className="!h-11 text-xs"
+                  text={`Create Robot`}
+                />
+              </div>
+            </form>
+          );
+        } else if (responseRobot?.physicalInstance) {
+          return (
+            <div className="w-full h-full flex flex-col gap-2 items-center justify-center">
+              <img
+                className="w-12 mx-auto pt-10"
+                src="/svg/general/loading.svg"
+                alt="Loading..."
+              />
+              <span className="text-sm text-layer-light-900">
+                Virtual Robot State:{" "}
+                {responseRobot?.robotClusters[0]?.robotStatus}
+              </span>
+              <span className="text-sm text-layer-light-900">
+                Physical Robot State:{" "}
+                {responseRobot?.robotClusters[1]?.robotStatus}
+              </span>
+              <span className="text-sm text-layer-light-900">
+                Please wait while we create your robot. This may take a few
+                minutes.
+              </span>
+            </div>
+          );
+        } else {
+          return (
+            <div className="w-full h-full flex flex-col gap-2 items-center justify-center">
+              <img
+                className="w-12 mx-auto pt-10"
+                src="/svg/general/loading.svg"
+                alt="Loading..."
+              />
+              <span className="text-sm text-layer-light-900">
+                Virtual Robot State:{" "}
+                {responseRobot?.robotClusters[0]?.robotStatus}
+              </span>
+              <span className="text-sm text-layer-light-900">
+                Please wait while we create your robot. This may take a few
+                minutes.
+              </span>
+            </div>
+          );
+        }
+      })()}
     </Fragment>
   );
 }
