dfb54d3dfb69aa6403cb471f1cc51a9c3b66a1d7
gokhangunduz <mail@gokhangunduz.com.tr>
2023-06-01 18:21:03 +0300

    refactor(robotInterfaces): :tada: Update useCreateRobot, IRobotBuildSteps, IRobotWorkspaceRepository and IRobotWorkspace interfaces

--- src/interfaces/robotInterfaces.ts
+++ src/interfaces/robotInterfaces.ts
@@ -10,27 +10,42 @@ export interface IRobotWorkspaces {
 
 export interface IRobotWorkspace {
   name: string;
-  workspaceDistro: "HUMBLE" | "GALACTIC" | "FOXY" | "";
+  workspaceDistro: "IRON" | "HUMBLE" | "GALACTIC" | "FOXY" | "";
   robotRepositories: IRobotWorkspaceRepository[];
 }
 
-interface IRobotWorkspaceRepository {
+export interface IRobotWorkspaceRepository {
   name: string;
   url: string;
   branch: string;
 }
 
 export interface IRobotBuildSteps {
-  steps: IRobotBuildStep[];
+  buildManagerName: string;
+  robotBuildSteps: IRobotBuildStep[];
 }
 export interface IRobotBuildStep {
   name: string;
   workspace: string;
-  isScriptCode: boolean;
-  code: string;
+  isCommandCode: boolean;
+  command: string;
+  script: string;
+  instancesName: any[];
 }
 
 export interface IuseCreateRobot {
   robotData: IRobotData;
   setRobotData: React.Dispatch<React.SetStateAction<IRobotData>>;
+  handleAddWorkspaceStep: (formik: any) => void;
+  handleRemoveWorkspaceStep: (formik: any, workspaceIndex: number) => void;
+  handleAddRepositoryToWorkspaceStep: (
+    formik: any,
+    workspaceIndex: number
+  ) => void;
+  handleRemoveRepositoryFromWorkspaceStep: (
+    formik: any,
+    workspaceIndex: number,
+    repositoryIndex: number
+  ) => void;
+  handleAddBuildStep: (formik: any) => void;
 }
