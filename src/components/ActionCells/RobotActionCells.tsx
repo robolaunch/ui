de75331fcad5561bf24497eda4ac2e542886d2fe
gokhangunduz <mail@gokhangunduz.com.tr>
2023-06-02 11:44:21 +0300

    feat(RobotActionCells): :tada: Add robot action cell

---
+++ src/components/ActionCells/RobotActionCells.tsx
@@ -0,0 +1,39 @@
+import React, { Fragment, ReactElement, useState } from "react";
+import Button from "../Button/Button";
+import { BiTrash } from "react-icons/bi";
+import DeleteRobotModalModal from "../../modals/DeleteRobotModal";
+
+interface IRobotActionCells {
+  data: any;
+  reload: () => void;
+}
+
+export default function RobotActionCells({
+  data,
+  reload,
+}: IRobotActionCells): ReactElement {
+  const [isDeleteModalVisible, setIsDeleteModalVisible] =
+    useState<boolean>(false);
+
+  console.log(data);
+
+  return (
+    <Fragment>
+      <div className="card flex gap-4 float-right">
+        <Button
+          className={`!w-8 !h-8 !bg-transparent !border !border-red-600`}
+          text={<BiTrash className="text-red-600" />}
+          onClick={() => setIsDeleteModalVisible(true)}
+        />
+      </div>
+      {isDeleteModalVisible && (
+        <DeleteRobotModalModal
+          data={data || undefined}
+          reload={reload}
+          handleCloseModal={() => setIsDeleteModalVisible(false)}
+          visibleModal={isDeleteModalVisible}
+        />
+      )}
+    </Fragment>
+  );
+}
