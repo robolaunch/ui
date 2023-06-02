975d8bddbaf3964ad1fc16ec696c5365304acc99
gokhangunduz <mail@gokhangunduz.com.tr>
2023-06-02 11:44:59 +0300

    feat(RobotServicesCell): :tada: Add robot services cell

---
+++ src/components/Cells/RobotServicesCell.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement } from "react";
+import InputToggle from "../InputToggle/InputToggle";
+
+interface IRobotServicesCell {
+  data: any;
+  states: any;
+}
+
+export default function RobotServicesCell({
+  data,
+  states,
+}: IRobotServicesCell): ReactElement {
+  return (
+    <div className="flex gap-4">
+      <div className="flex items-center">
+        <span className="text-xs font-medium">ROS Bridge:</span>
+        <InputToggle checked={states?.isEnabledRosBridge} onChange={() => {}} />
+      </div>
+      <div className="flex items-center">
+        <span className="text-xs font-medium">IDE:</span>
+        <InputToggle checked={states?.isEnabledRosBridge} onChange={() => {}} />
+      </div>
+      <div className="flex items-center">
+        <span className="text-xs font-medium">VDI:</span>
+        <InputToggle checked={states?.isEnabledRosBridge} onChange={() => {}} />
+      </div>
+    </div>
+  );
+}
