aa2bdf1fe3aa75785516807ed2ac9687ef460ec8
gokhangunduz <mail@gokhangunduz.com.tr>
2023-06-14 17:23:52 +0300

    fix(CreateRobotFormLaunchStepItem): tada: bug fix on key props

--- src/components/CreateRobotFormLaunchStepItem/CreateRobotFormLaunchStepItem.tsx
+++ src/components/CreateRobotFormLaunchStepItem/CreateRobotFormLaunchStepItem.tsx
@@ -190,6 +190,7 @@ export default function CreateRobotFormLaunchStepItem({
           ]?.robotLmEnvs?.map((env: any, envIndex: number) => {
             return (
               <CreateRobotFormEnvItem
+                key={envIndex}
                 formik={formik}
                 launchStepIndex={launchStepIndex}
                 envIndex={envIndex}
