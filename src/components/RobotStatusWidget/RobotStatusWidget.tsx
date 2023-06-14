545091d1f80249622613bab744a9326b4b695774
gokhangunduz <mail@gokhangunduz.com.tr>
2023-06-14 18:43:41 +0300

    refactor(RobotStatusWidget): :tada: Update widget

--- src/components/RobotStatusWidget/RobotStatusWidget.tsx
+++ src/components/RobotStatusWidget/RobotStatusWidget.tsx
@@ -14,6 +14,8 @@ export default function RobotStatusWidget({
   responseBuildManager,
   responseLaunchManagers,
 }: IRobotStatusWidget): ReactElement {
+  console.log(responseLaunchManagers);
+
   return (
     <WidgetLayout
       title={`Robot Status`}
@@ -21,9 +23,9 @@ export default function RobotStatusWidget({
       icon={<VscHistory size={20} className="text-layer-light-700" />}
     >
       <div className="flex items-center justify-around w-full h-full">
+        {/* Workspace */}
         <div className="flex flex-col gap-6 items-center justify-center">
-          <span>Workspace Manager Status</span>
-
+          <span>Workspace Manager</span>
           <div className="flex flex-col justify-center items-center gap-2">
             {!responseRobot ? (
               <div className="w-full h-full flex flex-col gap-2 items-center justify-center">
@@ -36,26 +38,64 @@ export default function RobotStatusWidget({
             ) : responseRobot?.robotCluster?.filter(
                 (robot: any) => robot?.robotStatus !== "EnvironmentReady"
               )?.length ? (
-              <FiAlertCircle />
+              <FiAlertCircle size={28} className="text-red-500" />
             ) : (
               <FiCheckCircle size={28} className="text-green-500" />
             )}
             <span className="text-sm">
-              {responseRobot?.robotCluster?.filter(
-                (robot: any) => robot?.robotStatus !== "EnvironmentReady"
-              )?.length
-                ? `Error`
+              {!responseRobot
+                ? "Loading..."
+                : responseRobot?.robotCluster?.filter(
+                    (robot: any) => robot?.robotStatus !== "EnvironmentReady"
+                  )?.length
+                ? responseRobot?.robotCluster?.filter(
+                    (robot: any) => robot?.robotStatus !== "EnvironmentReady"
+                  )[0]?.robotStatus
                 : "Ready"}
             </span>
           </div>
         </div>
-        <div className="flex flex-col gap-4">
-          <div>icon</div>
-          <span>Build Manager Status</span>
+        {/* Workspace */}
+
+        {/* Build */}
+        <div className="flex flex-col gap-6 items-center justify-center">
+          <span>Build Manager</span>
+          <div className="flex flex-col justify-center items-center gap-2">
+            {!responseBuildManager ? (
+              <div className="w-full h-full flex flex-col gap-2 items-center justify-center">
+                <img
+                  className="w-12 mx-auto pt-10"
+                  src="/svg/general/loading.svg"
+                  alt="Loading..."
+                />
+              </div>
+            ) : responseBuildManager?.robotClusters?.filter(
+                (robot: any) => robot?.buildManagerStatus !== "EnvironmentReady"
+              )?.length ? (
+              <FiAlertCircle size={28} className="text-red-500" />
+            ) : (
+              <FiCheckCircle size={28} className="text-green-500" />
+            )}
+            <span className="text-sm">
+              {!responseBuildManager
+                ? "Loading..."
+                : responseBuildManager?.robotClusters?.filter(
+                    (robot: any) =>
+                      robot?.buildManagerStatus !== "EnvironmentReady"
+                  )?.length
+                ? responseBuildManager?.robotClusters?.filter(
+                    (robot: any) =>
+                      robot?.buildManagerStatus !== "EnvironmentReady"
+                  )[0]?.buildManagerStatus
+                : "Ready"}
+            </span>
+          </div>
         </div>
+        {/* Build */}
+
         <div className="flex flex-col gap-4">
           <div>icon</div>
-          <span>Launch Manager Status</span>
+          <span>Launch Manager</span>
         </div>
       </div>
     </WidgetLayout>
