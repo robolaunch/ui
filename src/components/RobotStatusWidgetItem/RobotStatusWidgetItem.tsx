2ec50a0e4bd18deb4ebc069d613082d2a25b6407
gokhangunduz <mail@gokhangunduz.com.tr>
2023-06-15 18:03:46 +0300

    feat(RobotStatusWidgetItem): :tada: Add robot status widget item

---
+++ src/components/RobotStatusWidgetItem/RobotStatusWidgetItem.tsx
@@ -0,0 +1,52 @@
+import React, { ReactElement } from "react";
+import { FiAlertCircle, FiCheckCircle } from "react-icons/fi";
+
+interface IRobotStatusWidgetItem {
+  title: string;
+  loading: boolean;
+  state: string;
+  stateTextLoading: boolean;
+  stateText: string;
+}
+
+export default function RobotStatusWidgetItem({
+  title,
+  loading,
+  state,
+  stateTextLoading,
+  stateText,
+}: IRobotStatusWidgetItem): ReactElement {
+  return (
+    <div className="flex flex-col gap-6 items-center justify-center font-medium text-sm text-layer-dark-600">
+      <span>{title}</span>
+      <div className="flex flex-col justify-center items-center gap-2">
+        {loading ? (
+          <div className="w-full h-full flex flex-col gap-2 items-center justify-center">
+            <img
+              className="w-12 mx-auto pt-10"
+              src="/svg/general/loading.svg"
+              alt="Loading..."
+            />
+          </div>
+        ) : state === "success" ? (
+          <FiCheckCircle size={28} className="text-green-500" />
+        ) : state === "warning" ? (
+          <FiAlertCircle size={28} className="text-yellow-500" />
+        ) : (
+          <FiAlertCircle size={28} className="text-red-500" />
+        )}
+
+        <div className="flex items-center">
+          {stateTextLoading && (
+            <img
+              className="w-8"
+              src="/svg/general/loading.svg"
+              alt="Loading..."
+            />
+          )}
+          <span className="text-xs">{stateText}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
