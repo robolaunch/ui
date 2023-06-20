3732c6044d3fe57a996cab660b921f7fa4b41c97
gokhangunduz <mail@gokhangunduz.com.tr>
2023-06-20 16:42:49 +0300

    fix(openapi-generator.js): :tada: Fixed dot.env config error

--- openapi-generator.js
+++ openapi-generator.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const { exec } = require("child_process");
 
 const command = `sudo npx @openapitools/openapi-generator-cli generate -i ${process.env.REACT_APP_BACKEND_URL}/q/openapi?format=json -g typescript-axios -o src/api/openapi/`;
