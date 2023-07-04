2a4ef8d44be23e8d9381f0c41cf9a8b836b2422e
gokhangunduz <mail@gokhangunduz.com.tr>
2023-07-04 15:49:15 +0300

    feat(WorkspacesCell): :tada: Add workspace viewer component

---
+++ src/components/Cells/WorkspacesCell.tsx
@@ -0,0 +1,26 @@
+import React, { ReactElement } from "react";
+import TextwithTitle from "../TextwithTitle/TextwithTitle";
+
+interface IWorkspacesCell {
+  workspaces: any[];
+}
+
+export default function WorkspacesCell({
+  workspaces,
+}: IWorkspacesCell): ReactElement {
+  console.log(workspaces);
+
+  return (
+    <div className="flex flex-col items-center gap-2">
+      {workspaces?.map((workspace: any) => {
+        return (
+          <div className="flex gap-2 border border-layer-light-200 w-fit py-1.5 px-4 rounded">
+            <TextwithTitle title="Name:" text={workspace?.name} />
+            <TextwithTitle title="Branch:" text={workspace?.branch} />
+            <TextwithTitle title="URL:" text={workspace?.url} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
