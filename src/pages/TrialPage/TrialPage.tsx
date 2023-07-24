751607ec5f99428d3a23f0808389ecbf3807185b
gokhangunduz <mail@gokhangunduz.com.tr>
2023-07-24 15:46:39 +0300

    refactor(trial-page): :tada: update trial page

--- src/pages/TrialPage/TrialPage.tsx
+++ src/pages/TrialPage/TrialPage.tsx
@@ -1,11 +1,8 @@
 import React, { ReactElement } from "react";
 import TrialTimer from "../../components/TrialTimer/TrialTimer";
-import MarketplaceRobotTemplateItem from "../../components/MarketplaceRobotTemplateItem/MarketplaceRobotTemplateItem";
-import Paginate from "../../components/Paginate/Paginate";
 import TrialStateViewer from "../../components/TrialStateViewer/TrialStateViewer";
 import TrialFeedback from "../../components/TrialFeedback/TrialFeedback";
-import MarketplaceSortableBar from "../../components/MarketplaceSortableBar/MarketplaceSortableBar";
-import marketplaceData from "../../mock/marketplace.json";
+import MarketplaceGrid from "../../components/MarketplaceGrid/MarketplaceGrid";
 
 export default function TrialPage(): ReactElement {
   return (
@@ -15,18 +12,7 @@ export default function TrialPage(): ReactElement {
         <TrialStateViewer layoutClassName="row-span-2" />
         <TrialFeedback layoutClassName="row-span-3" />
       </div>
-      <div className="col-span-full lg:col-span-8 xl:col-span-9 2xl:col-span-10 h-full">
-        <div className="grid grid-cols-3 lg:grid-cols-6 2xl:grid-cols-9 gap-6 transition-all duration-500">
-          <MarketplaceSortableBar />
-
-          {marketplaceData.map((template: any, index: number) => {
-            return (
-              <MarketplaceRobotTemplateItem key={index} template={template} />
-            );
-          })}
-        </div>
-        <Paginate />
-      </div>
+      <MarketplaceGrid />
     </div>
   );
 }
