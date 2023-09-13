605fa5c60a6c5e75d69b1986cf311799c55403d8
gokhangunduz <mail@gokhangunduz.com.tr>
2023-09-13 10:04:47 +0300

    refactor (Input Checkbox): :tada: update checkbox style and add label

--- src/components/InputCheckbox/InputCheckbox.tsx
+++ src/components/InputCheckbox/InputCheckbox.tsx
@@ -20,15 +20,23 @@ export default function InputCheckbox({
   disabled,
 }: InputCheckboxProps): ReactElement {
   return (
-    <input
-      ref={ref}
-      disabled={disabled}
-      className={`scale-150 outline-none accent-primary ${className}`}
-      onChange={onChange}
-      onBlur={onBlur}
-      name={name}
-      type="checkbox"
-      checked={value}
-    />
+    <div className="flex gap-2">
+      <label
+        className="min-w-fit flex flex-wrap gap-1 text-xs font-medium text-layer-light-700"
+        htmlFor={name}
+      >
+        {name}
+      </label>
+      <input
+        ref={ref}
+        disabled={disabled}
+        className={`scale-150 outline-none accent-primary ${className}`}
+        onChange={onChange}
+        onBlur={onBlur}
+        name={name}
+        type="checkbox"
+        checked={value}
+      />
+    </div>
   );
 }
